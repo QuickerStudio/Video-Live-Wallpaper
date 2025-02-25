8e3f653ee9da8326e4b40bdc11c76efa1b216b61
Quicker <QuickerStudio@Gmail.com>
2025-02-26 00:58:21 +0800

    1
    1

--- app/src/main/java/moe/cyunrei/videolivewallpaper/utils/FileUtils.kt
+++ app/src/main/java/moe/cyunrei/videolivewallpaper/utils/FileUtils.kt
@@ -8,26 +8,16 @@ import java.nio.channels.FileChannel
 
 object FileUtils {
     fun copyFile(src: File?, dst: File?) {
-        var fileInputStream: FileInputStream? = null
-        var fileOutputStream: FileOutputStream? = null
-        var fileChannelInput: FileChannel? = null
-        var fileChannelOutput: FileChannel? = null
-        try {
-            fileInputStream = FileInputStream(src)
-            fileOutputStream = FileOutputStream(dst)
-            fileChannelInput = fileInputStream.channel
-            fileChannelOutput = fileOutputStream.channel
-            fileChannelInput.transferTo(0, fileChannelInput.size(), fileChannelOutput)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            try {
-                fileInputStream?.close()
-                fileChannelInput?.close()
-                fileOutputStream?.close()
-                fileChannelOutput?.close()
-            } catch (e: IOException) {
-                e.printStackTrace()
+        if (src == null || dst == null) return
+        FileInputStream(src).use { fileInputStream ->
+            FileOutputStream(dst).use { fileOutputStream ->
+                val fileChannelInput = fileInputStream.channel
+                val fileChannelOutput = fileOutputStream.channel
+                try {
+                    fileChannelInput.transferTo(0, fileChannelInput.size(), fileChannelOutput)
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
             }
         }
     }
